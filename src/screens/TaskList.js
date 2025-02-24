ecbf84613247ef408073d6f046f0b0e38836d7b6
RafaDiogoJ <rafabbta@hotmail.com>
2025-02-24 20:32:53 -0300

    Inserção imagem e inicio da estilização

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,9 +1,50 @@
-import {Text, View} from "react-native";
+import { View, Text, ImageBackground, StyleSheet } from "react-native";
 
-export default function TaskList(){
+import todayImage from '../../assets/imgs/today.jpg'
+
+export default function TaskList() {
     return(
-        <View>
-            <Text>TaskList</Text>
+        <View style={styles.container}>
+            <ImageBackground source={todayImage} style={styles.background}>
+                <View style={styles.titleBar}>
+                    <Text style={styles.title}>Hoje</Text>
+                    <Text style={styles.subtitle}>24 de Fevereiro</Text>
+                </View>
+
+
+            </ImageBackground>
+            <View style={styles.taskList}>
+                <Text>Task #01</Text>
+            </View>
+
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        flex: 3,
+    },
+    taskList: {
+        flex: 7,
+        backgroundColor: '#ff5d57'
+    },
+    titleBar:{
+        flex:1,
+        justifyContent: 'flex-end'
+    },
+    title:{
+        color: 'white',
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+    subtitle:{
+        color: 'white',
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30,
+    }
+})
